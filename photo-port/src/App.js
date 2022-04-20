8b5ea5a1fa33b2018c61b0b7218e4c5290deec4c
katredford <katharine.redford@gmail.com>
2022-04-20 17:54:23 -0500

    add photo-port

---
+++ photo-port/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+
+
+function App() {
+  return (
+    <div >
+      <Nav></Nav>
+      <main>
+        <About>
+
+        </About>
+    </main>
+    </div>
+  );
+}
+
+export default App;
