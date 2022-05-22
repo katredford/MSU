bac3d57af88aa592a058f2fa8e5cae0bbd1fc2a4
katredford <katharine.redford@gmail.com>
2022-05-21 22:56:55 -0500

    add prompt user questions

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -21,19 +21,103 @@
 // printProfileData(profileDataArgs);
 
 //9.2
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
 
-// console.log(name, github);
-// console.log(generatePage(name, github));
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-  if (err) throw err;
 
-  console.log("portfolio complete! check out index.html to see the output!");
-});
+// fs.writeFile("index.html", generatePage(name, github), err => {
+//   if (err) throw err;
+
+//   console.log("portfolio complete! check out index.html to see the output!");
+// });
+
+//9.3
+const inquirer = require('inquirer');
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+const promptUser = () => {
+
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'what is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+
+};
+promptUser().then(answers => console.log(answers));
+  
+const promptProject = () => {
+  console.log(`
+    =====================
+    Add a New Project
+    =====================
+    
+    `);
+  
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
