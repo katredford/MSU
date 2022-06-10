7a2615366b42e3a84a81c74e951ab71eb3fc80f4
katredford <katharine.redford@gmail.com>
2022-06-09 22:32:21 -0500

    start 12.2.7

--- election_app/server.js
+++ election_app/server.js
@@ -28,17 +28,78 @@ app.get('/', (req, res) => {
   });
 });
 
-//err is error response
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  //err is error response
 //rows is the database query response
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
 });
 
+// GET a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
